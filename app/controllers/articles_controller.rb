057eab2c3f3ac965da395bfb92913b9d2591def6
Ryan Johnson <ryanjohnson@Ryans-MacBook-Pro.local>
2024-06-19 12:23:16 -0400

    full tutotrial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,26 +1,51 @@
 class ArticlesController < ApplicationController
-
     def index
-        @articles = Article.all
+      @articles = Article.all
     end
-    
-    def new
-    end
-
-    def show
-        @article = Article.find(params[:id])
-    end
-
-    def create
-        @article = Article.new(article_params)
    
-        @article.save
+    def show
+      @article = Article.find(params[:id])
+    end
+   
+    def new
+      @article = Article.new
+    end
+   
+    def edit
+      @article = Article.find(params[:id])
+    end
+   
+    def create
+      @article = Article.new(article_params)
+   
+      if @article.save
         redirect_to @article
+      else
+        render 'new'
+      end
+    end
+   
+    def update
+      @article = Article.find(params[:id])
+   
+      if @article.update(article_params)
+        redirect_to @article
+      else
+        render 'edit'
+      end
+    end
+   
+    def destroy
+    puts "hello"
+      @article = Article.find(params[:id])
+      puts "hello"
+      @article.destroy
+   
+      redirect_to articles_path
     end
    
     private
-
     def article_params
-        params.require(:article).permit(:title, :text)
+      params.require(:article).permit(:title, :text)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get 'welcome/index'
  
-  resources :articles
+  resources :articles do
+    member do
+      delete :destroy
+    end
+  end
+  
 
   root 'welcome#index'
 end
